49733731848f257943130466a0d25fc006d42f29
Priya Rawat <rawatpriya0610@gmail.com>
2023-04-17 15:23:13 +0530

    admin added

---
+++ admin/dashboard.php
@@ -0,0 +1,129 @@
+<?php
+
+include '../components/connect.php';
+
+if(isset($_COOKIE['tutor_id'])){
+   $tutor_id = $_COOKIE['tutor_id'];
+}else{
+   $tutor_id = '';
+   header('location:login.php'); //user redirected to login.php page using header() function.
+}
+
+$select_contents = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ?");
+$select_contents->execute([$tutor_id]);
+$total_contents = $select_contents->rowCount();
+
+$select_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
+$select_playlists->execute([$tutor_id]);
+$total_playlists = $select_playlists->rowCount();
+
+$select_likes = $conn->prepare("SELECT * FROM `likes` WHERE tutor_id = ?");
+$select_likes->execute([$tutor_id]);
+$total_likes = $select_likes->rowCount();
+
+$select_comments = $conn->prepare("SELECT * FROM `comments` WHERE tutor_id = ?");
+$select_comments->execute([$tutor_id]);
+$total_comments = $select_comments->rowCount();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+   
+<section class="dashboard">
+
+   <h1 class="heading">dashboard</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>welcome!</h3>
+         <p><?= $fetch_profile['name']; ?></p> <!--welcome message that displays the user's name (retrieved from the $fetch_profile variable)-->
+         <a href="profile.php" class="btn">view profile</a>  <!-- and a button to view their profile.-->
+      </div>
+
+      <div class="box">
+         <h3><?= $total_contents; ?></h3>
+         <p>total contents</p>
+         <a href="add_content.php" class="btn">add new content</a>
+      </div>
+
+      <div class="box">
+         <h3><?= $total_playlists; ?></h3>
+         <p>total playlists</p>
+         <a href="add_playlist.php" class="btn">add new playlist</a>
+      </div>
+
+      <div class="box">
+         <h3><?= $total_likes; ?></h3>
+         <p>total likes</p>
+         <a href="contents.php" class="btn">view contents</a>
+      </div>
+
+      <div class="box">
+         <h3><?= $total_comments; ?></h3>
+         <p>total comments</p>
+         <a href="comments.php" class="btn">view comments</a>
+      </div>
+
+      <div class="box">
+         <h3>quick select</h3>
+         <p>login or register</p>
+         <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include '../components/footer.php'; ?>
+
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
+<!-- 
+A welcome message that displays the user's name (retrieved from the $fetch_profile variable) and a button to view their profile.
+The total number of contents on the site (retrieved from the $total_contents variable) and a button to add new content.
+The total number of playlists on the site (retrieved from the $total_playlists variable) and a button to add a new playlist.
+The total number of likes on the site (retrieved from the $total_likes variable) and a button to view contents.
+The total number of comments on the site (retrieved from the $total_comments variable) and a button to view comments.
+A "quick select" section with two buttons to either login or register for the site. 
+
+
+
+
+-->
