c819c5391f7fb1502f9a84d602aba3fcf5054a44
Priya Rawat <rawatpriya0610@gmail.com>
2023-04-25 15:29:16 +0530

    new changes added

--- components/connect.php
+++ components/connect.php
@@ -1,29 +1,20 @@
-<!-- <?php
+<?php
 
    $db_name = 'mysql:host=localhost;dbname=course_db';
    $user_name = 'root';
    $user_password = '';
 
-   $conn = new PDO($db_name, $user_name, $user_password);  // Obj of pdo class(dbname,)
+   $conn = new PDO($db_name, $user_name, $user_password);
 
    function unique_id() {
       $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-      $rand = array();   //initialize empty array
+      $rand = array();
       $length = strlen($str) - 1;
       for ($i = 0; $i < 20; $i++) {
-          $n = mt_rand(0, $length);  //mt_rand() function is used to generate a random number between 0 and the $length variable (inclusive), which is used as an index to retrieve a character from the $str string. The character is then added to the $rand array.
+          $n = mt_rand(0, $length);
           $rand[] = $str[$n];
       }
-      return implode($rand);  //join the elements of the $rand array together into a string
+      return implode($rand);
    }
-   
-//on calling 'unique_id()' function it genrates a random string of 20 alphanumeric chars
-//and can be useful for generating passwords
 
-/*1. connection
-  2. run SQL query
-      $conn -> query(SQL Query)
-      
-   3. close connection
-   $conn = null;*/
-?> --> 
+?>
