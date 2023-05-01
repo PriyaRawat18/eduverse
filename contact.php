57810ce38bfb3a10005e54c92fe912a36d6a1083
yashg <yash.gupta2361@gmail.com>
2023-05-01 11:00:38 +0530

    php updated

--- contact.php
+++ contact.php
@@ -76,25 +76,26 @@ if(isset($_POST['submit'])){
 
    <div class="box-container">
 
-      <div class="box">
-         <i class="fas fa-phone"></i>
-         <h3>phone number</h3>
-         <a href="tel:1234567890">123-456-7890</a>
-         <a href="tel:1112223333">111-222-3333</a>
-      </div>
+   <div class="box">
+                <i class="fas fa-phone"></i>
+                <h3>Phone number</h3>
+                <a href="tel:123456789">7275649441</a>
+                <a href="tel:1112223333">9027779036</a>
+            </div>
 
-      <div class="box">
-         <i class="fas fa-envelope"></i>
-         <h3>email address</h3>
-         <a href="mailto:shaikhanas@gmail.com">shaikhanas@gmail.come</a>
-         <a href="mailto:anasbhai@gmail.com">anasbhai@gmail.come</a>
-      </div>
+            <div class="box">
+                <i class="fas fa-envelope"></i>
+                <h3>email address</h3>
+                <a href="mailto:yash.gupta2361@gmail.com">Yash Gupta</a>
+                <a href="mailto:mathpalharshita45@gmail.com">Harshita Mathpal</a>
+                <a href="mailto:rawatpriya0610@gmail.com">Priya Rawat</a>
+            </div>
 
-      <div class="box">
-         <i class="fas fa-map-marker-alt"></i>
-         <h3>office address</h3>
-         <a href="#">flat no. 1, a-1 building, jogeshwari, mumbai, india - 400104</a>
-      </div>
+            <div class="box">
+                <i class="fas fa-map-marker-alt"></i>
+                <h3>office address</h3>
+                <a href="#">AVC colony,Agra (282007)</a>
+            </div>
 
 
    </div>
